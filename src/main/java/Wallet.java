9198f6182a42fbc6b8ce43f26f12440d6e9e0768
Louise McCafferty <mccafferty_@hotmail.com>
2022-11-21 14:27:32 +0000

    completed mvp

---
+++ src/main/java/Wallet.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Wallet {
+    private String name;
+    private ArrayList<IScan> walletItems;
+//    private ArrayList<DebitCard> debitCards;
+//    private ArrayList<LoyaltyCard> loyaltyCards;
+//    private ArrayList<Ticket> tickets;
+
+    public Wallet(String name) {
+        this.name = name;
+        this.walletItems = new ArrayList<IScan>();
+//        this.debitCards = new ArrayList<DebitCard>();
+//        this.loyaltyCards = new ArrayList<LoyaltyCard>();
+//        this.tickets = new ArrayList<Ticket>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfItems() {
+        return this.walletItems.size();
+    }
+
+    public void addItem(IScan item) {
+        this.walletItems.add(item);
+    }
+
+//    public void addItem(DebitCard item) {
+//        this.walletItems.add(item);
+//    }
+//
+//    public void addItem(LoyaltyCard item) {
+//        this.walletItems.add(item);
+//    }
+//
+//    public void addItem(Ticket item) {
+//        this.walletItems.add(item);
+//    }
+
+}
