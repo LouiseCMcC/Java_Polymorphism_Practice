884c1bb0913adf52abb5563ff254f489c87bfddc
Louise McCafferty <mccafferty_@hotmail.com>
2022-11-21 14:47:18 +0000

    completed extensions

--- src/main/java/CreditCard.java
+++ src/main/java/CreditCard.java
@@ -1,25 +1,26 @@
-public class CreditCard implements IScan {
-    private String cardNumber;
-    private String expiryDate;
-    private int securityNumber;
+public class CreditCard extends Card implements IScan {
+//    private String cardNumber;
+//    private String expiryDate;
+//    private int securityNumber;
 
     public CreditCard(String cardNumber, String expiryDate, int securityNumber) {
-        this.cardNumber = cardNumber;
-        this.expiryDate = expiryDate;
-        this.securityNumber = securityNumber;
+        super(cardNumber, expiryDate, securityNumber);
+//        this.cardNumber = cardNumber;
+//        this.expiryDate = expiryDate;
+//        this.securityNumber = securityNumber;
     }
 
-    public String getCardNumber() {
-        return this.cardNumber;
-    }
-
-    public String getExpiryDate() {
-        return this.expiryDate;
-    }
-
-    public int getSecurityNumber() {
-        return this.securityNumber;
-    }
+//    public String getCardNumber() {
+//        return this.cardNumber;
+//    }
+//
+//    public String getExpiryDate() {
+//        return this.expiryDate;
+//    }
+//
+//    public int getSecurityNumber() {
+//        return this.securityNumber;
+//    }
 
     public String scan() {
         return "Payment Successful";
